e79b669640626aeb705600c76de96a63453934c1
Joseph Ruff <joegruff16@gmail.com>
2024-08-17 15:07:34 -0400

    Added all of my dependences and added some starter code.

---
+++ src/Food.jsx
@@ -0,0 +1,12 @@
+function Food() {
+  const food = "Orange";
+  const food2 = "Banana";
+  return (
+    <ul>
+      <li>Apple</li>
+      <li>{food1}</li>
+    </ul>
+  );
+}
+
+export default Food;
